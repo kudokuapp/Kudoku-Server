26f25c40b2b330b08ba74b5c87f3ea30abbfd8f7
Furqon Wilogo <furqon@kudoku.id>
2023-03-05 07:17:28 +0000

    Update 4 files

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -3,7 +3,7 @@ export * from './Auth';
 export * from './Profile';
 export * from './CashAccount';
 export * from './DebitAccount';
-// export * from './EWallet';
+export * from './EWallet';
 export * from './EMoney';
 export * from './Refresh';
 export * from './Merchant';
